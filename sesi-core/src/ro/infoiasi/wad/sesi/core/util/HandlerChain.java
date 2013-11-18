a45a5e8aa4792fc903ad0b9af4607cc4c45892e4
Ana-Maria Daneliuc <anad@amazon.com>
2013-11-18 14:01:43 +0200

    A better version of HandlerChain

--- sesi-core/src/ro/infoiasi/wad/sesi/core/util/HandlerChain.java
+++ sesi-core/src/ro/infoiasi/wad/sesi/core/util/HandlerChain.java
@@ -1,11 +1,20 @@
 package ro.infoiasi.wad.sesi.core.util;
 
+
 public final class HandlerChain {
 
-    private HandlerChain(){}
+    private HandlerChain() {
 
-    public static <I, T, O>  O chain(I input, Handler<I, T> first, Handler<T, O> second) throws Exception {
-
-        return second.handle(first.handle(input));
     }
+
+    public static <I, T, O> Handler<I, O> chain(final Handler<I, T> first, final Handler<T, O> second) {
+
+        return new Handler<I, O>() {
+            @Override
+            public O handle(I input) throws Exception {
+                return second.handle(first.handle(input));
+            }
+        };
+    }
+
 }
