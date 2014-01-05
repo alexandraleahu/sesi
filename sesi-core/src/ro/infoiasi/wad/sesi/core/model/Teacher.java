dccb2c9f44b50e2093c23f82f7b0d77abe10071a
Ana-Maria Daneliuc <annouk.m@gmail.com>
2014-01-05 03:41:45 +0200

    added the teacher class

---
+++ sesi-core/src/ro/infoiasi/wad/sesi/core/model/Teacher.java
@@ -0,0 +1,70 @@
+package ro.infoiasi.wad.sesi.core.model;
+
+public class Teacher implements User, Resource {
+
+    private School school;
+    private int id;
+    private String description;
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public Role getRole() {
+        return Role.TEACHER;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Teacher teacher = (Teacher) o;
+
+        if (id != teacher.id) return false;
+        if (description != null ? !description.equals(teacher.description) : teacher.description != null) return false;
+        if (school != null ? !school.equals(teacher.school) : teacher.school != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = school != null ? school.hashCode() : 0;
+        result = 31 * result + id;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "school=" + school +
+                ", id=" + id +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
