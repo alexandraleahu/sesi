19b951b15284264e84de201fb206e1a1e0a92e79
Ana-Maria Daneliuc <annouk.m@gmail.com>
2014-01-25 14:22:44 +0200

    adding service authenticator

--- sesi-client/src/main/java/ro/infoiasi/wad/sesi/server/util/SparqlService.java
+++ sesi-client/src/main/java/ro/infoiasi/wad/sesi/server/util/SparqlService.java
@@ -2,6 +2,7 @@ package ro.infoiasi.wad.sesi.server.util;
 
 
 import com.hp.hpl.jena.query.*;
+import org.apache.jena.atlas.web.auth.ServiceAuthenticator;
 
 public class SparqlService {
     final static String serviceEndpoint = "http://localhost:5820/sesi/query/";
@@ -13,12 +14,12 @@ public class SparqlService {
 
     public static void main(String[] args) {
         String comNameQuery =
-                "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> " +
-                "PREFIX sesiSchema:<http://www.infoiasi.ro/wad/objects/sesi/> " +
-                "select ?name {<http://rdf.freebase.com/ns/m.01fhg3> sesiSchema:name ?name } ";
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+                "PREFIX sesiSchema: <http://www.infoiasi.ro/wad/schemas/sesi/> " +
+                "select ?name where {<http://rdf.freebase.com/ns/m.01fhg3> sesiSchema:name ?name } ";
 //                " <http://rdf.freebase.com/ns/m.01fhg3> rdfs:seeAlso ?seeAlso}";
         Query query = QueryFactory.create(comNameQuery);
-        QueryExecution qe = QueryExecutionFactory.sparqlService(serviceEndpoint, query);
+        QueryExecution qe = QueryExecutionFactory.sparqlService(serviceEndpoint, query, new ServiceAuthenticator("admin", "admin".toCharArray()));
 
         try {
             ResultSet rs = qe.execSelect();
