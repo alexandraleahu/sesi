597ae29754dd5af426b01046646b1a0bdbe38cb3
Iulian Dolceanu <iulian.dolceanu@yahoo.com>
2014-01-27 05:14:13 +0200

    InternshipListView

--- sesi-client/src/main/java/ro/infoiasi/wad/sesi/client/ui/CompaniesPanel.java
+++ sesi-client/src/main/java/ro/infoiasi/wad/sesi/client/ui/CompaniesPanel.java
@@ -1,9 +1,11 @@
 package ro.infoiasi.wad.sesi.client.ui;
 
 import ro.infoiasi.wad.sesi.client.compositewidgets.InternshipEditor;
+import ro.infoiasi.wad.sesi.client.compositewidgets.InternshipListView;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.ui.Hyperlink;
 
 public class CompaniesPanel extends Tab {
@@ -19,7 +21,13 @@ public class CompaniesPanel extends Tab {
             this.addClickHandler(new ClickHandler() {
                 public void onClick(ClickEvent arg0) {
                     CompaniesPanel.this.leftPanel.clear();
-                    // CompaniesPanel.this.leftPanel.add(new InternshipsView());
+                    InternshipListView ilv = new InternshipListView();
+                    String currentUser = Cookies.getCookie("currentUser");
+                    if (currentUser != null && currentUser.length() > 0) {
+                        ilv.filterCompany(currentUser);
+                    }
+                    ilv.display();
+                    CompaniesPanel.this.leftPanel.add(ilv);
                 }
             });
         }
